4f0fcdaaa02cbe528e17b2e26b6444dd08d0055e
Dominic Mc Neil <dsmcneil3@yahoo.com>
2019-11-05 16:31:08 +0000

    My script regarding the coveyer belt system.

---
+++ Assets/Transform_Object.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transform_Object : MonoBehaviour
+{
+    public GameObject Input;
+    public Transform Output;
+    public Transform Stop;
+    public GameObject Product;
+    public float step;
+    public float Distance;
+
+    void Update()
+    {
+        Distance = Vector3.Distance(transform.position, Output.position);
+        step = 1f;
+
+        Input.transform.position = Vector3.MoveTowards(Input.transform.position, Output.position, step);
+
+        if (Distance >= 0.1f && Input.activeSelf == true)
+        {
+            Instantiate(Product);
+            Input.SetActive(false);
+        }
+    }
+}
